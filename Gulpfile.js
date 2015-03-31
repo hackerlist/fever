8991ecbed37df05792c9165e919d34885d29ad47
valish <andrew@vali.sh>
2015-03-31 03:26:46 -0700

    adding production gulp task

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,9 +3,20 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
 
-gulp.task('sass', function () {
+gulp.task('dev', function () {
   gulp.src('./scss/fever.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(gulp.dest('./css'))
+})
+
+gulp.task('prod', function () {
+  gulp.src('./scss/fever.scss')
+    .pipe(sass({
+      sourceComments: 'none',
+      outputStyle: 'compressed'
+    }))
+    .pipe(rename('fever.min.css'))
+    .pipe(gulp.dest('./css'))
 })
